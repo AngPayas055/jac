e5c8e341a01e28bbd115d0bd9844be51a4243336
Jhon Rey <jhonreymendiola@gmail.com>
2022-09-09 15:19:24 +0800

    path redircet to landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 // import { HeroesComponent } from './heroes/heroes.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: '/landing', pathMatch: 'full' },
   { path: 'landing', loadChildren: () => import('./landing/landing.module').then(m => m.LandingModule) },
 ];
 
