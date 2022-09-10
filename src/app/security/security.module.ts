cb1953f184eb97aa968309c14a9d5e1db1a17b58
Jhon Rey <jhonreymendiola@gmail.com>
2022-09-11 00:01:06 +0800

    responsive login with material input

--- src/app/security/security.module.ts
+++ src/app/security/security.module.ts
@@ -9,6 +9,9 @@ import { SecurityRoutingModule } from './security-routing.module';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 // Flex Layout
 // import { FlexLayoutModule } from '@angular/flex-layout';
@@ -28,6 +31,9 @@ import { LoginComponent } from './login/login.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule
     // FlexLayoutModule
   ]
 })
