4d68f500912cea1af19f818c206a02812bb6a3eb
Jhon Rey <jhonreymendiola@gmail.com>
2022-11-20 20:51:54 +0800

    prod setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   app_url: 'http://localhost:4200',
-  api_url: 'http://localhost:8000/api',
+  api_url: 'http://jacapi-online.preview-domain.com/api',
   deployment_target: 'LOCAL',
-  storage_url: 'http://localhost:8000',
+  storage_url: 'http://jacapi-online.preview-domain.com',
 };
