d8609919c5c356ed4ec602ded3403b8a09405bf9
Jhon Rey <jhonreymendiola@gmail.com>
2022-11-21 12:07:46 +0800

    connect to local

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,9 @@
 export const environment = {
   production: true,
-  app_url: 'http://localhost:4200',
-  api_url: 'https://jacapi-online.preview-domain.com/api',
+  app_url: 'https://jmendiola.com',
+  // api_url: 'https://jacapi-online.preview-domain.com/api',
+  api_url: 'http://localhost:8000/api',
   deployment_target: 'LOCAL',
-  storage_url: 'https://jacapi-online.preview-domain.com',
+  // storage_url: 'https://jacapi-online.preview-domain.com',
+  storage_url: 'http://localhost:8000',
 };
